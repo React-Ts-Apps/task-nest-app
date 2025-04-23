036a715fcf0a437cd973c709e6f7ab7652e56993
ShijiBijo84 <shijib.bijo@gmail.com>
2025-04-23 14:25:21 +0200

    #2-add data and types for task

---
+++ task-front-end/src/data/Tasks.ts
@@ -0,0 +1,20 @@
+export const initialTasks = [
+  {
+    id: 1,
+    title: 'Create Theme',
+    assignee: 'Shiji Bijo',
+    status: 'to-do',
+  },
+  {
+    id: 2,
+    title: 'Create Columns',
+    assignee: 'Isabel',
+    status: 'to-do',
+  },
+  {
+    id: 3,
+    title: 'Create Columns',
+    assignee: 'Rita',
+    status: 'doing',
+  },
+];
