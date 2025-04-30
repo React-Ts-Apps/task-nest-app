22228a1656cc6386513b63f1f1476ac9c6ffba16
ShijiBijo84 <shijib.bijo@gmail.com>
2025-04-30 11:19:24 +0200

    #14-drag and drop tasks implemented

--- task-front-end/src/components/task/TaskBoard.tsx
+++ task-front-end/src/components/task/TaskBoard.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { COLUMNS, INITIAL_TASKS } from '../../data/Tasks';
 import TaskColumn from './TaskColumn';
 import { TaskStatus } from '../../types/TaskProps';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
 
 type TaskColWrapperProps = {
-  status: TaskStatus;
+  $status: TaskStatus;
 };
 
 const TaskBoardContainer = styled.div`
@@ -16,25 +17,43 @@ const TaskBoardContainer = styled.div`
 `;
 const TaskColumnWrapper = styled.div<TaskColWrapperProps>`
   border-top: 2px solid
-    ${({ status }) =>
-      status === 'TODO' ? '#007acc' : status === 'IN_PROGRESS' ? '#ff9900' : '#28a745'};
+    ${({ $status }) =>
+      $status === 'TODO' ? '#007acc' : $status === 'IN_PROGRESS' ? '#ff9900' : '#28a745'};
 `;
 
 const TaskBoard = () => {
   const [tasks, setTasks] = useState(INITIAL_TASKS);
+
+  useEffect(() => {
+    console.log('Updated tasks:', tasks);
+  }, [tasks]);
+
+  const handleDragEnd = (e: DragEndEvent) => {
+    const { active, over } = e;
+    if (!active || !over) return;
+    const draggedTaskId = active.id; //identify task being dragged by id
+    const targetColumn = over.id as TaskStatus; //identify column by status
+
+    setTasks((tasks) =>
+      tasks.map((task) => (task.id == draggedTaskId ? { ...task, status: targetColumn } : task)),
+    );
+  };
+
   return (
-    <div>
-      <TaskBoardContainer>
-        {COLUMNS.map((col) => (
-          <TaskColumnWrapper key={col.id} status={col.id}>
-            <TaskColumn
-              column={col}
-              tasks={tasks.filter((task) => task.status == col.id)}
-            ></TaskColumn>
-          </TaskColumnWrapper>
-        ))}
-      </TaskBoardContainer>
-    </div>
+    <DndContext onDragEnd={handleDragEnd}>
+      <div>
+        <TaskBoardContainer>
+          {COLUMNS.map((col) => (
+            <TaskColumnWrapper key={col.id} $status={col.id}>
+              <TaskColumn
+                column={col}
+                tasks={tasks.filter((task) => task.status == col.id)}
+              ></TaskColumn>
+            </TaskColumnWrapper>
+          ))}
+        </TaskBoardContainer>
+      </div>
+    </DndContext>
   );
 };
 export default TaskBoard;
